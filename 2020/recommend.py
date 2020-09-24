758a7f4d9fe4ee7fbac337c683bdc191130334ad
suryaprakashmereddy <sreddy@acertaralabs.com>
2020-09-23 22:52:16 -0700

    setup

---
+++ 2020/recommend.py
@@ -0,0 +1,45 @@
+import collections
+import re
+
+from bs4 import BeautifulSoup
+
+from base import web
+
+
+def selection(response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    strong_tags = soup.find_all("strong")
+
+    players = []
+    words = ['Fantasy Suggestion', 'Captain', 'Vice-Captain']
+
+    for strong_tag in strong_tags:
+        if any(word in strong_tag.text for word in words):
+            team = cleanup(strong_tag.next_sibling)
+            players += team.split(",")
+
+    feq = collections.defaultdict(int)
+
+    for player in players:
+        feq[player] += 1
+
+    print({k: v for k, v in sorted(feq.items(), key=lambda item: item[1], reverse=True)})
+
+
+def cleanup(string):
+    team = string.replace('and', ',')
+    team = team.replace(u'\xa0', u'')
+    return re.sub('[!@#$: ]', '', team)
+
+
+def fantasy():
+    playing_xi = web()
+    playing_xi_url = playing_xi.find_fanatsy()
+
+    if playing_xi_url:
+        resp, status = playing_xi.read(playing_xi.base_url + playing_xi_url)
+        if status:
+            selection(resp)
+        else:
+            print('Please check again later')
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+Package setup
+"""
+
+import setuptools  # type: ignore
+
+setuptools.setup(
+    name="IPL",
+    version=2020,
+    author="Surya Mereddy",
+    author_email="suryamereddy@gmail.com",
+    description="Recommend Fantasy 11",
+    long_description="N/A",
+    url="https://medium.com/@surya.mereddy",
+    license="MIT",
+    platforms=["Linux", "Windows"],
+    packages=["IPL2020"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
